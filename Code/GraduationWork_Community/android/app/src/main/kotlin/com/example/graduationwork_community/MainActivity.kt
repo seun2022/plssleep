82e6365efeaaf9dfdeceec50a6412853eda781f0
Six_Tail <qowlgh1215@naver.com>
2024-06-01 16:25:11 +0900

    code파일 삭제

--- Code/GraduationWork_Community/android/app/src/main/kotlin/com/example/graduationwork_community/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.graduationwork_community
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- Code/Project/lib/themes/app_theme.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:project/themes/dark_theme.dart';
-import 'package:project/themes/light_theme.dart';
-
-class AppTheme {
-  static ThemeData light = lightTheme;
-  static ThemeData dark = darkTheme;
-}
